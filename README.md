1a28b064b4798c891bf00a6a88f310979b9ec0e9
Yanguk <heo.yanguk@gmail.com>
2022-10-15 16:55:54 +0900

    feat: react 초기 셋팅
    cra로 react 설치
    eslint, prettier, husky 설치

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Circle-Service-FE
+
+```js
+// Development Start
+
+npm install
+
+npm run prepare
+
+npm start
+
+// http://localhost:3000/
+```
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
+  printWidth: 80,
+  bracketSpacing: true,
+};
